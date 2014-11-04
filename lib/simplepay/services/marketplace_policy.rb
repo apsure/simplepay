a87e4ebe67df2acc019dafaf10bbf771a576f80d
Nathan Palmer <nathan@nathanpalmer.com>
2014-11-04 09:27:37 -0500

    Added signature version 2 to the marketplace policy button

--- lib/simplepay/services/marketplace_policy.rb
+++ lib/simplepay/services/marketplace_policy.rb
@@ -1,24 +1,24 @@
 module Simplepay
   module Services
-  
+
     ##
     # The Amazon Simple Pay Marketplace Policy service is used to allow sellers to acknowledge marketplace policy fees.
     # Only once a set policy has been agreed to will marketplace transactions be able to proceed.
     #
     # === Simple Pay Marketplace Policy Fields
-    # 
+    #
     # ==== Required Fields
-    # 
+    #
     # The following attributes are required when creating a Simple Pay Marketplace policy fee acceptance form
     # (in addition to those listed in +Simplepay::Service+):
-    # 
+    #
     # max_fixed_fee:: The maximum fixed fee that will be appended to transactions.
     # max_variable_fee:: The maximum variable fee (%) that will be calculated and added to transactions.
     # return_url:: Fully-qualified URL for where to send they buyer following payment.
     # reference_id:: A custom string used to identify this transaction, it will be returned with return data.
     #
     # === Example
-    # 
+    #
     #     (in your view, using the form helper)
     #
     #     <%= simplepay_form_for(:marketplace_policy, {
@@ -29,7 +29,7 @@ module Simplepay
     #     }) %>
     #
     class MarketplacePolicy < Service
-      
+
       # Fully-qualified URL for the production endpoint for the service.
       ENDPOINT_URL  = 'https://authorize.payments.amazon.com/cobranded-ui/actions/start'
 
@@ -37,9 +37,12 @@ module Simplepay
       SANDBOX_URL   = 'https://authorize.payments-sandbox.amazon.com/cobranded-ui/actions/start'
 
       required_field :access_key, :as => :caller_key
-      required_field :signature,  :as => :aws_signature
+      required_field :signature
+      required_field :signatureVersion,     :value  => '2'
+      required_field :signatureMethod,      :value  => 'HmacSHA256'
+
       required_field :account_id, :as => :caller_account_id
-      
+
       required_field :max_fixed_fee
       required_field :max_variable_fee
       required_field :return_url
@@ -50,8 +53,8 @@ module Simplepay
 
       # These fields are not currently utilized by the service
       required_field :recipient_pays_fee,    :value => 'True'
-      
+
     end
-    
+
   end
 end
